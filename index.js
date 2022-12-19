a7db42d80cc546aa2bfb121f5036d5531a673c56
Dikshant Deogade <dikshantdeogade72@gmail.com>
2022-12-19 12:16:23 +0530

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("workind")
+
+function myOppener(){
+    let x=document.getElementById("textnav");
+    x.style.right='0px';
+   
+}
+
+function myCloser(){
+    let x=document.getElementById("textnav");
+    x.style.right='-200px';
+}
