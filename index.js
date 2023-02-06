0bb1bf18494991f3ab0673a8f29b31aa6238ca11
Dikshant Deogade <dikshantdeogade72@gmail.com>
2023-02-06 22:35:33 +0530

    Updated..

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ function myCloser(){
 function mail(){
     
 }
+
+let x=1;
+function menu(){
+    if(x==0)
+    {
+    document.getElementById("menubar").innerHTML=`<i class="fa-solid fa-bars"></i>`;
+    document.getElementById("navbar").style.display=`none`;
+
+    x=1;
+    }
+    else{
+        document.getElementById("menubar").innerHTML=`<i class="fa-solid fa-xmark"></i>`;
+        
+        let z=document.getElementById("navbar");
+        z.style.display=`block`;
+        z.style.marginLeft=`75%`;
+        z.style.fontSize=`80%`;
+    //    let a=document.getElementById("navbar");
+    //    a.style.gap=`20px`;
+        
+        x=0;
+    }
+}
