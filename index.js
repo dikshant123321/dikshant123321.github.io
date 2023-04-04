fecdef8c602295080fd6aa3a4ca2978cb7dc967a
dikshant <dikshantdeogade72@gmail.com>
2023-04-04 13:46:01 +0530

    Updated Resume

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
-console.log("workind")
-
-function myOppener(){
-    let x=document.getElementById("textnav");
-    x.style.right='0px';
-   
-}
-
-function myCloser(){
-    let x=document.getElementById("textnav");
-    x.style.right='-200px';
-}
-
-function mail(){
-    
-}
-
-let x=1;
-function menu(){
-    if(x==0)
-    {
-    document.getElementById("menubar").innerHTML=`<i class="fa-solid fa-bars"></i>`;
-    document.getElementById("navbar").style.display=`none`;
-
-    x=1;
+	// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menubar');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
     }
-    else{
-        document.getElementById("menubar").innerHTML=`<i class="fa-solid fa-xmark"></i>`;
-        
-        let z=document.getElementById("navbar");
-        z.style.display=`block`;
-        z.style.marginLeft=`75%`;
-        z.style.fontSize=`80%`;
-    //    let a=document.getElementById("navbar");
-    //    a.style.gap=`20px`;
-        
-        x=0;
+    window.onscroll = () => {
+        navbar.classList.remove('active');
     }
-}
+     
